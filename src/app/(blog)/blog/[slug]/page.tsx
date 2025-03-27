3afd287273320813b51a0fd5d111c91b14220f68
sam-morgan <andersonhenderson45@gmail.com>
2025-03-27 00:32:38 -0400

    fixed lint

--- src/app/(blog)/blog/[slug]/page.tsx
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import { getAllPostSlugs, getPostBySlug } from '@/lib/posts'
 import { stringToDateString } from "@/lib"
-import { useMDXComponents } from '@/mdx-components'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
@@ -29,7 +28,7 @@ export default async function BlogPostPage({ params }: BlogPostParams) {
           <p>Posted on {stringToDateString(post.metadata.date)}{post.metadata.updateDate ? `, last updated ${stringToDateString(post.metadata.updateDate)}` : ''}</p>
         </div>
         <div style={{ marginTop: '3rem'}}>
-          <MDXRemote components={useMDXComponents({})} source={post.content} />
+          <MDXRemote source={post.content} />
         </div>
         <div style={{ marginTop: '3rem'}}>
           <Link className="right" href={"/blog"}>&larr; See other posts</Link>
--- src/app/(blog)/poem/[slug]/page.tsx
+++ src/app/(blog)/poem/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { useMDXComponents } from '@/mdx-components'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
@@ -28,7 +27,7 @@ export default async function PoemPage({ params }: PoemParams) {
           {poem.metadata.author && <p>By {poem.metadata.author}</p>}
         </div>
         <div style={{ marginTop: '3rem'}}>
-          <MDXRemote components={useMDXComponents({})} source={poem.content} />
+          <MDXRemote source={poem.content} />
         </div>
         <div style={{ marginTop: '3rem'}}>
           <Link className="right" href={"/misc/poetry"}>&larr; See other poems</Link>
