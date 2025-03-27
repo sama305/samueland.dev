858075717dff0f5b212a1943fdde067cf047008d
sam-morgan <andersonhenderson45@gmail.com>
2025-03-27 12:40:06 -0400

    consolidated poem/blog pages into shared component

--- src/app/(blog)/blog/[slug]/page.tsx
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { getAllPostSlugs, getPostBySlug } from '@/lib/posts'
 import { stringToDateString } from "@/lib"
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import Link from 'next/link'
 import { notFound } from 'next/navigation'
+import ComponentPageOutline from '../../../../../components/ComponentPageOutline'
 
 interface BlogPostParams {
   params: Promise<{
@@ -21,19 +21,12 @@ export default async function BlogPostPage({ params }: BlogPostParams) {
   if (!post) return notFound()
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1 style={{ lineHeight: '2rem' }}>{post.metadata.title}</h1>
-          <p>Posted on {stringToDateString(post.metadata.date)}{post.metadata.updateDate ? `, last updated ${stringToDateString(post.metadata.updateDate)}` : ''}</p>
-        </div>
-        <div style={{ marginTop: '3rem'}}>
-          <MDXRemote source={post.content} />
-        </div>
-        <div style={{ marginTop: '3rem'}}>
-          <Link className="right" href={"/blog"}>&larr; See other posts</Link>
-        </div>
-      </div>
-    </div>
+    <ComponentPageOutline
+      title={post.metadata.title}
+      subtitle={`Posted on ${stringToDateString(post.metadata.date)}${post.metadata.updateDate ? `, last updated ${stringToDateString(post.metadata.updateDate)}` : ''}`}
+      returnLink={{ to: '/blog', label: 'See other posts'}}
+    >
+      <MDXRemote source={post.content} />
+    </ ComponentPageOutline>
   )
 }
