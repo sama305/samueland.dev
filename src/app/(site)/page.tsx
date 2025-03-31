e554def85cbf4cc0899deef0f115a9cffba2bafe
sam-morgan <andersonhenderson45@gmail.com>
2025-03-31 14:34:43 -0400

    fix: minor wording changes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,13 +4,11 @@ export default function About() {
       <div className="left">
         <p>
           Hello. My name is Samuel Morgan Anderson, and I'm a computer science professional living in Miami, Florida,
-          where I currently intern at <a href="https://www.watscoventures.com/">Watsco Ventures LLC</a>. I've also lived
-          in
-          Tallahassee, Florida, where I received a
-          Bachelor of Science in Computer Science from Florida State University in 2025.
+          where I currently intern at <a href="https://www.watscoventures.com/">Watsco Ventures LLC</a>. Currently, I am also enrolled at Florida State University where I am set to recieve a
+          Bachelor of Science in Computer Science from Florida State University in May of 2025.
         </p>
         <p>
-          My experience with computer science goes all the way back to my childhood; since then, I've worked with on solo projects, small group projects, and even with teams at large companies. Yet no matter the situation, I always focus on the ultimate goal: delivering high quality, polished software.
+          My experience with computer science goes all the way back to my childhood; since then, I've worked with on solo projects, small group projects, and even with teams at large companies. My ultimate goal with all of these projects is to deliver the highest quality software possible.
         </p>
         <p>
           Besides computer science, my interests include reading, writing, playing guitar and piano, studying Spanish, listening to lots of music, and making small games.
