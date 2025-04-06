a414a63ac521497d98cda8e0d57eb94718c0ee01
sam-morgan <andersonhenderson45@gmail.com>
2025-04-06 17:15:17 -0400

    changed wording a bit

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -18,10 +18,13 @@ export default function Projects() {
         </div>
         <div className="right">
           <p>
-            <a className="social" href="https://librewrite.com" target="_blank">Librewrite.com</a> is a free webapp intended for practicing creative writing. It facilitates the exercise of <i>free writing</i>, which is, according to <a href="https://en.wikipedia.org/wiki/Free_writing">Wikipedia</a>, "a prewriting technique practiced in academic environments, in which a person writes continuously for a set period of time with limited concern for rhetoric, conventions, and mechanics, sometimes working from a specific prompt provided by a teacher."
+            <a className="social" href="https://librewrite.com" target="_blank">Librewrite.com</a> is a free webapp for practicing creative writing. It facilitates the exercise of <i>free writing</i>, a method of practicing writing where you write nonstop for a set amount of time, sometimes working from a prompt.
           </p>
           <p>
             I initially discovered free writing in my LIT2000 class, and immediately wanted to continue the practice at home. However, I noticed there was a lack of websites which could administer free writing sessions, and since I didn't want to ask a friend to stand shouting out prompts for me, I began work on my own tool: Librewrite.com.
+          </p>
+          <p>
+            Pieces are written within timed rounds, with optional prompts for a greater challenge. Additionally, you can easily create/share custom rounds, which are rounds where the settings/prompts are chosen beforehand and can't be changed by anyone who plays it. This feature was specifically designed with teachers in mind, and makes sharing tailored configurations as easy as sending a link.
           </p>
         </div>
       </div>
@@ -72,10 +75,10 @@ export default function Projects() {
         </div>
         <div className="right">
           <p>
-            The aptly titled Order-Tracker is a webapp intended for compiling orders placed through a variety of online shops (e.g. Shopify, Amazon) into a single, centralized location. It was created with three others as the COP4521 final project. 
+            The aptly titled <a className="social" href="https://github.com/danieltdang/Order-Tracker" target="_blank">Order-Tracker</a> is a webapp intended for compiling orders placed through a variety of online shops (e.g. Shopify, Amazon) into a single, centralized location. It was created with three others as the COP4521 final project. 
           </p>
           <p>
-            Related emails are scraped for suitable order data, or the user can manually enter the order number. Each order can be in one of many states, such as shipped, delivered, or even cancelled. A main dashboard intuitively presents general data about current and past orders.
+            Related emails are scraped for suitable order data, or the user can optionally enter the order info manually. Each order has one of many statuses, such as shipped, delivered, or even cancelled. A main dashboard intuitively presents general data about current and past orders.
           </p>
           <p>
             Finally, users can "pay" (as this was a school project, it was only implemented as a proof of concept) for a premium subscription which grants access to more useful metrics/order data and the ability to interact with some orders.
