4d5bb5cc1b3f7de18c8549181f4c5e64ff251065
sam-morgan <andersonhenderson45@gmail.com>
2025-07-03 13:39:01 -0400

    added year separators to poetry section

---
+++ components/DatedLinkList.tsx
@@ -0,0 +1,41 @@
+import { DatedLinkListEntry } from "@/lib/types"
+import Link from "next/link"
+
+interface LinkListProps {
+  links: Array<DatedLinkListEntry>
+}
+
+export default function LinkList({ links }: LinkListProps) {
+  const datedLinks: { [key: number]: Array<DatedLinkListEntry> } = {}
+
+  links.forEach(l => {
+    const dateYear = new Date(l.date).getFullYear()
+    if (!datedLinks[dateYear]) {
+      datedLinks[dateYear] = [l]
+    }
+    else {
+      datedLinks[dateYear].push(l)
+    }
+  })
+
+  return (
+    <div>
+      {Object.keys(datedLinks).reverse().map((k => {
+        return (
+          <div key={k}>
+            <h2>{k}</h2>
+            <ul>
+            {datedLinks[parseInt(k)].map(({title, subtitle, to, date}) => {
+              return (
+                <li key={to}>
+                  <span><Link href={to}>{title}</Link>{subtitle ? subtitle : ''}</span>
+                </li>
+              )
+            })}
+            </ul>
+          </div>
+        )
+      }))}
+    </div>
+  )
+}
