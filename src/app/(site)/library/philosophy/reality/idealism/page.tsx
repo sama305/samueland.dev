4767e9a5edd8bcc2adf3f6e8dcef9b4bb75cccbb
Sam Anderson <andersonhenderson45@gmail.com>
2026-01-20 11:05:41 -0500

    fixes

--- src/app/(site)/library/philosophy/reality/idealism/page.tsx
+++ src/app/(site)/library/philosophy/reality/idealism/page.tsx
@@ -1,5 +1,4 @@
 import LibraryPage from "@/components/LibraryPage";
-import SamLink from "@/components/SamLink";
 
 export default function Reality() {
   return (
@@ -10,7 +9,7 @@ export default function Reality() {
         <p>Plato is perhaps the most famous idealist. He came up with the concept of <em>Forms</em>: the idea that all that we observe are only shadows or imperfect renditions of timeless, abstract, perfect Forms that exist outside our physical reality and even our own minds.</p>
         <h3>Allegory of the Cave</h3>
         <p>To explain this, he used the <em>Allegory of the Cave</em>. In this allegory, a group of people are chained within a cave, forced to look at shadows on the wall. They see all kinds of people and animals walking by, and mistake these shadows for reality. Eventually, one of the prisoners breaks free and escapes the cave. He sees reality as it actually was (i.e. Forms) and realizes the shadows were imperfect representations. He returns to the cave to try and rescue his fellow prisoners, but they don&#x27;t believe him and even threaten to kill him.</p>
-        <p>Besides the sociological end of this story,</p>
+        {/* <p>Besides the sociological end of this story,</p> */}
       </article>
     </LibraryPage>
   )  
