845a4f2031d41542f6ae38315a7341d11ec932f4
Sam Anderson <andersonhenderson45@gmail.com>
2026-01-20 14:18:41 -0500

    added some idealismstuff

--- src/app/(site)/library/philosophy/reality/idealism/page.tsx
+++ src/app/(site)/library/philosophy/reality/idealism/page.tsx
@@ -4,12 +4,18 @@ export default function Reality() {
   return (
     <LibraryPage title="Idealism" subtitle='“To be is to be perceived.”—George Berkeley'>
       <article>
-        <p><strong>Idealism </strong> encompassses many different ideas but the basic premise is that the observable universe exists as a construct within our mind; reality as we know it simply exists as ideas.</p>
-        <h2>Plato</h2>
-        <p>Plato is perhaps the most famous idealist. He came up with the concept of <em>Forms</em>: the idea that all that we observe are only shadows or imperfect renditions of timeless, abstract, perfect Forms that exist outside our physical reality and even our own minds.</p>
-        <h3>Allegory of the Cave</h3>
-        <p>To explain this, he used the <em>Allegory of the Cave</em>. In this allegory, a group of people are chained within a cave, forced to look at shadows on the wall. They see all kinds of people and animals walking by, and mistake these shadows for reality. Eventually, one of the prisoners breaks free and escapes the cave. He sees reality as it actually was (i.e. Forms) and realizes the shadows were imperfect representations. He returns to the cave to try and rescue his fellow prisoners, but they don&#x27;t believe him and even threaten to kill him.</p>
-        {/* <p>Besides the sociological end of this story,</p> */}
+        <p>Idealism encompassses many different ideas but the basic premise is that the observable universe exists as a construct within our mind; reality as we know it simply exists as ideas.</p>
+        <h2>Famous idealists</h2>
+        <h3>Plato</h3>
+        <p>Plato is one of the most famous idealists. His seminal achievement in idealism was coming up with the concept of <em>Forms</em>: the idea that all that we can observe are only imperfect projections of timeless, abstract, perfect Forms that exist outside our physical reality and even our own minds.</p>
+        <h4>Allegory of the Cave</h4>
+        <p>To explain this more easily, he came up with the <em>Allegory of the Cave</em>. In this allegory, a group of people are chained within a cave, forced to look at shadows on the wall. They see all kinds of people and animals walking by, and mistake these shadows for reality. Eventually, one of the prisoners breaks free and escapes the cave. He sees reality as it actually was (i.e. Forms) and realizes the shadows were imperfect representations. He returns to the cave to try and rescue his fellow prisoners, but they don&#x27;t believe him and even threaten to kill him.</p>
+        <p>Besides the sociological implications of this story (that those who can&#x27;t see the truth are angered by those who claim they can), the imagery makes Plato&#x27;s point quite clear: just as shadows are imperfect projections of more perfect objects, our world is composed of imperfect projections of perfect Forms.</p>
+        <h3>Hegel</h3>
+        <p>He was pretty famous too, but I don&#x27;t really know much about him. More research is needed here.</p>
+        <h3>George Berkeley</h3>
+        <p>Berkeley believed an even more strict version of idealism: that <em>nothing</em> material exists except for our minds. Everything you can perceive, from tables to chairs to doors, can only &quot;exist&quot; as it is being perceived. His theories and related ideas are often grouped under the blanket of <em>subjective idealism</em>.</p>
+        <p>My understanding is that he saw no distinction between what we see when we think of something and when we see something: in both cases we are perceiving it to bring it into existence.</p>
       </article>
     </LibraryPage>
   )  
