91081b85ee0fee202c3ef5f33156f3c98da9dd43
Sam Anderson <andersonhenderson45@gmail.com>
2026-01-23 22:05:50 -0500

    some stuff

--- src/app/(site)/library/music/songs/page.tsx
+++ src/app/(site)/library/music/songs/page.tsx
@@ -6,6 +6,54 @@ import { libraryBaseUrl } from "@/lib/navlists"
 import { DatedLinkListEntry } from "@/lib/types"
 import DatedLinkList from "@/components/DatedLinkList"
 
+
+/*
+ * TODO
+ *
+ * Songs missing from list:
+ * - Moby Dick (Jan or Mar 2025)
+ * - Valentine (Feb 2025)
+ * - Please, Please, Please (Jan 2025)
+ * - Have You Seen the River (May 2023)
+ * - Lockjaw John (Dec 2022)
+ * - Growing Our Love (Jun 2022)
+ * - Miss You (May 2025)
+ * - St. Augustine (May 2025)
+ * - Cobblestone (May 2025)
+ * - Panama (May 2025)
+ * - War (Sep 2025)
+ * - Everybody's Gotta Go to School (Sep 2025)
+ * - 10 Years (Sep 2025)
+ * - Somebody New (Sep 2025)
+ * - Queen (Oct 2025)
+ * - Whitecollar Blues (Oct 2025)
+ * - Signs of You (Nov 2025)
+ * - Under Sea, Under Stone (Nov 2025)
+ * - Blazing Blue (Nov 2025)
+ * - Love Me Soon (Dec 2025)
+ * - I Know What You're Dreaming Of (Dec 2025)
+ * - From Your Light (Dec 2025)
+ * - The Fool w/ Nick (Dec 2025)
+ * - Miss America w/ Nick (Dec 2025)
+ *
+ * Songs missing chords:
+ * - Earthlings
+ * - Pagans of Tolo
+ *
+ * Unfinished songs:
+ * - Circles (May 2025)
+ * - No Fear/Ants (May 2025)
+ * - The Man with the Cigar (Sep 2025)
+ * - The Saloon (Sep 2025)
+ * - Do You Hear Me (Oct 2025)
+ * - Prince of the Dead (Oct 2025)
+ * - Tin-Pan Times (Oct 2025)
+ * - I Know, I Know (Dec 2025)
+ * - Before November (Nov 2025)
+ * - New Orleans (Jan 2026)
+ * - Following the Sunshine
+ */
+
 export default function Songs() {
   const songs: Array<DatedLinkListEntry> = getAllSongs().map(({ metadata, slug }) => ({
     title: metadata.title,
